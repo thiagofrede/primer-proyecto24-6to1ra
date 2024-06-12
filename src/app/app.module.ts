0f873203061ad325a3ce2772e6a3b0485d275487
thiagofrede <thiagofredes1@gmail.com>
2024-06-12 10:00:39 -0300

    creamos un nuevo servicio para acceder al cloud firestore de firebase y definimos una coleccion para los usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-//IMPORTAMOS COMPONENTES GLOBALES
 import { SharedModule } from './modules/shared/shared.module';
 
 
@@ -19,14 +16,13 @@ import {AngularFireStorageModule} from '@angular/fire/compat/storage';//trabaja 
 @NgModule({
   declarations: [
     AppComponent,
-    
   ],
-  imports: [
+  imports:[
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
-    AngularFireModule.initialilizeApp(environment.firebaseConfig),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireAuthModule,
     AngularFireStorageModule,
   ],
